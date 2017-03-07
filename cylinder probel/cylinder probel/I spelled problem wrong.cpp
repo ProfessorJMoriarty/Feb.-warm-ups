3939816bfdb3e5e0057f15d18eb8ae81a4a5199f
Dr. J. Moriarty <Jennifer_Moriarty@dpsk12.org>
2017-03-07 09:28:38 -0700

    see comments
    see comments

--- cylinder probel/cylinder probel/I spelled problem wrong.cpp
+++ cylinder probel/cylinder probel/I spelled problem wrong.cpp
@@ -7,10 +7,11 @@ int main() {
 
 	cout << "what the Radius of the cylinder?" << endl;
 	cin >> Radius;
-	cout << "What's the heights of the cone" << endl;
+	cout << "What's the heights of the cone" << endl; //change to cylinder
 	cin >> height;
 
-	sum = (Radius *height);
+	sum = (Radius *height); //check this formula!
 	cout << "The volume of the cylinder is." << sum << endl;
 
+	//please push a flowchart picture
 }
